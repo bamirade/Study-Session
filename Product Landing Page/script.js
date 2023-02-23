1a0bbd20662d3deebf229db5ec8d34c7fdc705f4
bamirade <118993260+bamirade@users.noreply.github.com>
2023-02-23 10:17:03 +0800

    Add files via upload

---
+++ Product Landing Page/script.js
@@ -0,0 +1,75 @@
+const navLinks = document.querySelectorAll('.nav-link');
+
+function scrollToSection(event) {
+event.preventDefault();
+const section = document.querySelector(event.target.hash);
+section.scrollIntoView({ behavior: 'smooth' });
+}
+
+navLinks.forEach(link => {
+link.addEventListener('click', scrollToSection);
+});
+
+window.addEventListener('scroll', function() {
+    let topBar = document.querySelector('#header');
+    if (window.scrollY > 0) {
+        topBar.style.backgroundColor = '#fff';
+    }
+    else {
+        topBar.style.backgroundColor = 'rgba(255,255,255,0.9)';
+    }
+  });
+
+  const images = ['image4.png', 'image2.png', 'image3.png'];
+  let index = 0;
+  
+  const imageObjects = [];
+  for (const image of images) {
+    const img = new Image();
+    img.src = `media/artists works/${image}`;
+    imageObjects.push(img);
+  }
+  
+  setInterval(() => {
+    const imageUrl = `url('media/artists works/${images[index]}')`;
+    document.getElementById('overview').style.backgroundImage = `url('media/kanban_movie_mask.png'), ${imageUrl}`;
+    index = (index + 1) % images.length;
+  }, 5000);
+
+  const sections = document.querySelectorAll('.fade-in');
+
+const checkVisibility = () => {
+  sections.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const sectionBottom = section.getBoundingClientRect().bottom;
+    const isVisible = (sectionTop < window.innerHeight) && (sectionBottom >= 0);
+
+    if (isVisible) {
+      section.classList.add('visible');
+    }
+  });
+}
+
+window.addEventListener('scroll', checkVisibility);
+
+// Select a single element
+const banner = document.getElementById('banner');
+
+// Select multiple elements
+const navLinks = document.querySelectorAll('nav a');
+const featuresList = document.querySelectorAll('#features li');
+
+// Modify an element's text
+banner.querySelector('h1').textContent = 'New Product Name';
+
+// Modify an element's styles
+banner.style.backgroundColor = '#f0f0f0';
+
+// Add a button and event listener for button clicks
+const btn = document.createElement('button');
+btn.textContent = 'Click Me';
+banner.appendChild(btn);
+
+btn.addEventListener('click', function() {
+  alert('Button Clicked!');
+});
